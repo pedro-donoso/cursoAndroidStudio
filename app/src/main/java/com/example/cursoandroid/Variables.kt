16d394622a3c1ff190523a3c96f21b4fdc980604
pedro-donoso <pedro.donoso.r@usach.cl>
2024-07-25 19:59:18 -0400

    agrego rama desarrollo

---
+++ app/src/main/java/com/example/cursoandroid/Variables.kt
@@ -0,0 +1,43 @@
+package com.example.cursoandroid
+
+//VALORES
+/**
+ *Valores Numéricos
+ */
+fun main(){
+
+    //Int
+    val age: Int = 36
+
+    //Long
+    val example:Long = 1000000
+
+    //Float
+    val floatExample: Float = 3.14f
+
+    //Double
+    val doubleExample: Double = 3.141592653589793
+
+    /**
+     *Valores Alfanuméricos
+     */
+
+//Char
+    val charExample1: Char = 'p'
+    val charExample2: Char = '2'
+    val charExample3: Char = '@'
+
+    //String
+    val stringExample: String = "Hello World"
+
+    /**
+     *Valores Booleanos
+     */
+
+    val booleanExample4: Boolean = true
+    val booleanExample5: Boolean = false
+
+   println(doubleExample)
+}
+
+
