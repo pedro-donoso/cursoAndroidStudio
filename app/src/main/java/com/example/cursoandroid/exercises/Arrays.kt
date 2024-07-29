7042ea16783b6a9177d1ba25f19c4cc0912909bd
pedro-donoso <pedro.donoso.r@usach.cl>
2024-07-29 14:10:42 -0400

    creo iteraciones arrays

---
+++ app/src/main/java/com/example/cursoandroid/exercises/Arrays.kt
@@ -0,0 +1,34 @@
+package com.example.cursoandroid.exercises
+
+fun main() {
+    //Indice 0-6
+    //Tamaño 7
+    val weekDays = arrayOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+
+ //   println(weekDays[0])
+ //   println(weekDays.size)
+
+    //Tamaños
+    if (weekDays.size >= 8) {
+  //      println(weekDays[7])
+    } else {
+  //      println("No hay mas valores en el array")
+    }
+
+    //Modificar valores
+    weekDays[0] = "Feliz lunes"
+  //  println(weekDays[0])
+
+    //Bucles
+    for(position in weekDays.indices){
+       // println("He pasado por aqui $position")
+    }
+
+    for((position, value) in weekDays.withIndex()){
+        //println("La posicion $position, contiene $value")
+    }
+
+    for (weekday in weekDays){
+        println("Ahora es $weekday")
+    }
+}
